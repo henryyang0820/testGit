7afa07fd384b375218f8ae96d9c817ae33ef180a
“henryyang0820\ngit config --global user.email “y1045033116@qq.com <1045033116@qq.com>
2018-05-22 18:30:57 +0800

    update statistics by excel

--- myGit.py
+++ myGit.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+import os
 from git import Repo
-repo = Repo('E:\inquiryFund')
-print(repo.bare)
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+excel_path = os.path.abspath(base_path+'\myfundStatistics')
+
+print(excel_path)
+repo = Repo('E:/testGit')
 print(repo.is_dirty())
-print(repo.active_branch)
+index = repo.index
+index.add(['myGit.py'])
+index.add(['myfundStatistics.xlsx'])
+index.commit('update statistics by excel')
+remote = repo.remote()
+print(remote.name)
+remote.push()
